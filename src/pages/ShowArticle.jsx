f02d10da83a94c9a47560629131ca9015af8375a
David Robert <contact@david-robert.fr>
2022-12-15 16:14:36 +0100

    boilerplate crud

--- src/pages/ShowArticle.jsx
+++ src/pages/ShowArticle.jsx
@@ -1,23 +1,31 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const ShowArticle = () => {
+  const { id } = useParams();
+  const [article, setArticle] = useState({});
 
-    const { id } = useParams();
+  const fakeArticleFromApi = {
+    id: 1,
+    title: "Article 1",
+    content: "Contenu de l'article 1",
+  };
 
-    const article = {
-        id: 1,
-        title: 'Article 1',
-        content: 'Contenu de l\'article 1'
-    };
+  setArticle(fakeArticleFromApi);
 
-    // const articleFromApiResponse = await fetch('http://localhost:5000/api/articles/' + id);
-    // const articleFromApi = await articleFromApiResponse.json(); 
+  //   useEffect(() => {
+  //     (async () => {
+  //       const response = await fetch("http://localhost:5000/api/articles/" + id);
+  //       const article = await response.json();
+  //       setArticle(article);
+  //     })();
+  //   }, [id]);
 
-    return (
-        <article>
-            <h1>{article.title}</h1>
-        </article>
-    )
-}
+  return (
+    <article>
+      <h1>{article.title}</h1>
+    </article>
+  );
+};
 
 export default ShowArticle;
